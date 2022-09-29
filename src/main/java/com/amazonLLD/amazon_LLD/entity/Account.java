b2a7fedceeb742c3a217a35d63ec66921c009d3a
DivyanshuRath <divrathore@deloitte.com>
2022-09-29 23:53:11 +0530

    created Account class and AccountStatus enum

---
+++ src/main/java/com/amazonLLD/amazon_LLD/entity/Account.java
@@ -0,0 +1,61 @@
+package com.amazonLLD.amazon_LLD.entity;
+
+import com.amazonLLD.amazon_LLD.enumration.AccountStatus;
+
+public class Account {
+    private String userName;
+    private String password;
+    AccountStatus accountStatus;
+    private String address;
+    private String email;
+    private String phoneNumber;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AccountStatus getAccountStatus() {
+        return accountStatus;
+    }
+
+    public void setAccountStatus(AccountStatus accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
---
+++ src/main/java/com/amazonLLD/amazon_LLD/enumration/AccountStatus.java
@@ -0,0 +1,9 @@
+package com.amazonLLD.amazon_LLD.enumration;
+
+public enum AccountStatus {
+    ACTIVE,
+    BLOCKED,
+    BANNED,
+    COMPROMIZED,
+    UNKNOWN
+}
