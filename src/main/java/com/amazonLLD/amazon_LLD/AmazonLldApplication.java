bc7c00241c06c705f79903f1884dc91ccfaef8ac
DivyanshuRath <divrathore@deloitte.com>
2022-09-29 23:19:41 +0530

    Initial commit

---
+++ src/main/java/com/amazonLLD/amazon_LLD/AmazonLldApplication.java
@@ -0,0 +1,13 @@
+package com.amazonLLD.amazon_LLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonLldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonLldApplication.class, args);
+	}
+
+}
